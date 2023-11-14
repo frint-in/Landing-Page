24872c5e6852cb272d34495033bf7fa1a79d2dd9
Ritav Kashyap <ritav20-237@aec.ac.in>
2023-11-14 15:35:11 +0530

    auth added

--- Frint/src/App.jsx
+++ Frint/src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Auth from "./components/auth/Auth";
+
 
 function App() {
-  return <>Frint</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
