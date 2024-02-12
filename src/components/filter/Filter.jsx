eac4919f2445d931b51d012a78198eea1409dc83
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-02-13 02:02:49 +0530

    filter little bit done

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,174 +1,100 @@
-import React, { useState, useEffect } from "react";
-import './filter.scss'
-
-import {
-  FaChevronCircleRight,
-  FaChevronCircleLeft,
-} from "react-icons/fa";
-import { LuSchool2 } from "react-icons/lu";
-import { GoGear } from "react-icons/go";
-import { VscBellDot } from "react-icons/vsc";
-import { GoPeople } from "react-icons/go";
-import { BsBarChart } from "react-icons/bs";
-import { PiMoney } from "react-icons/pi";
-import { IoIosPeople } from "react-icons/io";
-import { GiTeacher } from "react-icons/gi";
-import { AiOutlineSchedule } from "react-icons/ai";
+import React, { useState } from "react";
+import "./filter.scss";
+import { FaFilter, FaPowerOff, FaLock } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { PiSuitcaseSimpleBold } from "react-icons/pi";
+import { RxDashboard } from "react-icons/rx";
 import { NavLink } from "react-router-dom";
 
+const menuItem = [
+  {
+    path: "/company-dashboard",
+    name: "Dashboard",
+    icon: <RxDashboard />,
+  },
+  {
+    path: "/company-profile",
+    name: "Profile",
+    icon: <CgProfile />,
+  },
+  {
+    path: "/internship",
+    name: "Internship",
+    icon: <PiSuitcaseSimpleBold />,
+  },
+  {
+    path: "/company-password",
+    name: "Change Password",
+    icon: <FaLock />,
+  },
+];
+
 const Filter = ({ children }) => {
+  const [selectedTab, setSelectedTab] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
 
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 450);
-
-  useEffect(() => {
-      const handleResize = () => {
-          setIsSmallScreen(window.innerWidth < 450);
-      };
-
-      window.addEventListener('resize', handleResize);
-
-      return () => {
-          window.removeEventListener('resize', handleResize);
-      };
-  }, []);
-
-  const [searchInput, setSearchInput] = useState('');
-
-  const handleInputChange = (event) => {
-    let value = event.target.value;  
-    setSearchInput(value);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
   };
 
-  const menuItem1 = [
-      {
-          path: "/dashboard",
-          name: "Dashboard",
-          icon: <BsBarChart />,
-      },
-      {
-          path: "/academy-management",
-          name: "Academy Management",
-          icon: <LuSchool2 />,
-      },
-  ];
-  const menuItem2 = [
-      {
-          path: "/student",
-          name: "Student",
-          icon: <GoPeople />,
-      },
-      {
-          path: "/fee-payment",
-          name: "Fee Payment",
-          icon: <PiMoney />,
-      },
-      {
-          path: "/enquiries",
-          name: "Enquiries",
-          icon: <IoIosPeople />,
-      },
-      {
-          path: "/coaches",
-          name: "Coaches",
-          icon: <GiTeacher />,
-      },
-      {
-          path: "/schedule",
-          name: "Schedule",
-          icon: <AiOutlineSchedule />,
-      },
-  ];
-  const menuItem3 = [
-      {
-          path: "/settings",
-          name: "Settings",
-          icon: <GoGear />,
-      },
-      {
-          path: "/notification",
-          name: "Notification",
-          icon: <VscBellDot />,
-      },
-  ];
-
   return (
-      <div className="Filter">
-          <div className="Sidebar">
+    <div className={`Filter ${isOpen ? "open" : ""}`}>
+      <div className="Sidebar">
+        <div
+          style={{ width: isOpen ? "25rem" : "4.5rem" }}
+          className="container"
+        >
+          <div className="sidebarTop">
+            <div className="top_section">
+              <NavLink to={"/"} key={1}>
+                <div
+                  style={{ display: isOpen ? "block" : "none" }}
+                  className="logo"
+                >
+                  Filter
+                </div>
+              </NavLink>
               <div
-                  style={{ width: isOpen ? "15rem" : "4.5rem" }}
-                  className="container"
+                className="bars"
+                style={{ marginLeft: isOpen ? "6rem" : "0.5rem" }}
               >
-                  <div className="sidebarTop" style={{ alignItems: isOpen ? "start" : "center" }}>
-                      <div className="top_section">
-                          <div
-                              style={{ marginLeft: isOpen ? "11rem" : "0" }}
-                              className="bars"
-                          >
-                              <FaChevronCircleRight
-                                  onClick={toggle}
-                                  className="abc"
-                                  style={{ display: isOpen ? "none" : "block", color: "#fff" }}
-                              />
-                              <FaChevronCircleLeft
-                                  onClick={toggle}
-                                  className="cba"
-                                  style={{ display: isOpen ? "block" : "none", color: "#fff" }}
-                              />
-                          </div>
-                      </div>
-                      <div className="head" style={{ display: isOpen ? "block" : "none" }}
-                      >Main Menu</div>
-                      {menuItem1.map((item, index) => (
-                          <NavLink to={item.path} key={index} className="link">
-                              <div className="icon">{item.icon}</div>
-                              <div
-                                  style={{ display: isOpen ? "block" : "none" }}
-                                  className="link_text"
-                              >
-                                  {item.name}
-                              </div>
-                          </NavLink>
-                      ))}
-                      <div className="head2" style={{ display: isOpen ? "block" : "none" }}
-                      >More</div>
-                      <div className="link" style={{ display: isOpen ? "block" : "none" }}>
-                          <input className='search' value={searchInput} onChange={handleInputChange} placeholder="🔍 Search students by name" />
-                      </div>
-                      {menuItem2.map((item, index) => (
-                          <NavLink to={item.path} key={index} className="link">
-                              <div className="icon">{item.icon}</div>
-                              <div
-                                  style={{ display: isOpen ? "block" : "none" }}
-                                  className="link_text"
-                              >
-                                  {item.name}
-                              </div>
-                          </NavLink>
-                      ))}
-                      {isSmallScreen && (
-                          <div>
-                              {menuItem3.map((item, index) => (
-                                  <NavLink to={item.path} key={index} className="link">
-                                      <div className="icon">{item.icon}</div>
-                                      <div
-                                          style={{ display: isOpen ? 'block' : 'none' }}
-                                          className="link_text"
-                                      >
-                                          {item.name}
-                                      </div>
-                                  </NavLink>
-                              ))}
-                          </div>
-                      )}
-                  </div>
+                <FaFilter
+                  onClick={toggleSidebar}
+                  className="abc"
+                  style={{ display: isOpen ? "none" : "block", color: "#fff" }}
+                />
+                <FaFilter
+                  onClick={toggleSidebar}
+                  className="cba"
+                  style={{ display: isOpen ? "block" : "none", color: "#fff" }}
+                />
               </div>
-              <main>{children}</main>
+            </div>
+            <hr
+              className="hr-bar"
+              style={{ display: isOpen ? "block" : "none" }}
+            />
+            {menuItem.map((item, index) => (
+              <NavLink
+                to={item.path}
+                key={index}
+                className={`link ${selectedTab === item.path ? "active" : ""}`}
+              >
+                <div className="icon">{item.icon}</div>
+                <div
+                  style={{ display: isOpen ? "block" : "none" }}
+                  className="link_text"
+                >
+                  {item.name}
+                </div>
+              </NavLink>
+            ))}
           </div>
+        </div>
+        <main>{children}</main>
       </div>
+    </div>
   );
 };
 
-
-export default Filter
+export default Filter;
