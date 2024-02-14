2f9e354344a60c6b6d04d19bc8e7e847d35393f4
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-02-15 00:05:19 +0530

    card init

--- src/pages/internship/Internship.jsx
+++ src/pages/internship/Internship.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
-import './internship.scss'
-import Filter from '../../components/filter/Filter'
+import React, { useState } from "react";
+import "./internship.scss";
+import Filter from "../../components/filter/Filter";
+import { VscSettings } from "react-icons/vsc";
+import Card from "../../components/internship-card/Card";
+
 
 const Internship = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [sort, setSort] = useState();
+
+  const handleOptionClick = (option) => {
+    setCategory(option);
+    setDropdownOpen(false);
+  };
+
+  const options = [
+    "Sales Event",
+    "Tech Event",
+    "Design Event",
+    "Marketing Event",
+  ];
+  
   return (
-    <div className='Internship'>
+    <div className="Internship">
       <div className="header"></div>
       <Filter />
+      <div className="headerdiv">
+        <div className="headtext">Recommended Internships</div>
+        <div className="Dropdown">
+          <div
+            className={`dropdown-header ${dropdownOpen ? "open" : ""}`}
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+          >
+            {sort ? sort : "Sorted by"}
+            <div className="custom-down-icon">
+              <VscSettings />
+            </div>
+          </div>
+          {dropdownOpen && (
+            <div className="options">
+              {options.map((option) => (
+                <div
+                  key={option}
+                  className="option"
+                  onChange={() => handleOptionClick(option)}
+                  onClick={() => handleOptionClick(option)}
+                  value={sort}
+                  required
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="body">
+        <Card />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Internship
+export default Internship;
