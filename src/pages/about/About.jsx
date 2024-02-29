c7621f7d2519ae1297d868be05a0415561b68a31
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-02-29 21:46:45 +0530

    updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./about.scss";
-<<<<<<< HEAD
 const About = () => {
-  const logos = [
-    "/1.png",
-    "/2.png",
-    "/3.png",
-    "/4.png",
-    "/5.png",
-  ];
+  const logos = ["/1.png", "/2.png", "/3.png", "/4.png", "/5.png"];
 
   return (
     <div className="About">
+      <div className="header"></div>
+
       <div className="about-us">
         <div className="heading">
           <h1>About US</h1>
@@ -38,67 +33,64 @@ const About = () => {
 
         <div className="services">
           <h1>Our Services</h1>
-          <p>Bringing talent together, empowering career and shaping the future of work</p>    
+          <p>
+            Bringing talent together, empowering career and shaping the future
+            of work
+          </p>
           <div className="card-container">
             <div className="card">
               <img src="freelance.png" alt="" />
               <h2>Internship</h2>
-              <p>Discover a curated selection of top-notch internships in various fields</p>
+              <p>
+                Discover a curated selection of top-notch internships in various
+                fields
+              </p>
             </div>
             <div className="card">
               <img src="intership.png" alt="" />
               <h2>Freelancing</h2>
-              <p>Earn extra income and build your skills wiht flexible and remote freelance projects</p>
+              <p>
+                Earn extra income and build your skills wiht flexible and remote
+                freelance projects
+              </p>
             </div>
             <div className="card">
               <img src="skill.png" alt="" />
               <h2>Skill Development</h2>
-              <p>Improve your skills and stay up to date with the latest industry trends through online courses and training programs</p>
+              <p>
+                Improve your skills and stay up to date with the latest industry
+                trends through online courses and training programs
+              </p>
             </div>
-          </div>            
+          </div>
         </div>
 
         <div className="collaborations">
           <h1>Our Collaborations</h1>
-        <div>
-          <div className="slider">
-            <div className="slide-track">
-              {[...Array(100)].map((_, repeatIndex) => (
-                // Duplicating logos for repetition
-                <React.Fragment key={repeatIndex}>
-                  {logos.map((logo, index) => (
-                    <div key={index} className="slide">
-                      <img
-                        src={logo}
-                        alt={`Logo ${index + 1}`}
-                        height={100}
-                        width={250}
-                      />
-                    </div>
-                  ))}
-                </React.Fragment>
-              ))}
+          <div>
+            <div className="slider">
+              <div className="slide-track">
+                {[...Array(100)].map((_, repeatIndex) => (
+                  // Duplicating logos for repetition
+                  <React.Fragment key={repeatIndex}>
+                    {logos.map((logo, index) => (
+                      <div key={index} className="slide">
+                        <img
+                          src={logo}
+                          alt={`Logo ${index + 1}`}
+                          height={100}
+                          width={250}
+                        />
+                      </div>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-      
-
-    </div>
     </div>
   );
 };
-
-=======
-
-const About = () => {
-  return (
-    <div className="About">
-      <div className="header"></div>
-    </div>
-  );
-};
-
->>>>>>> 348f68c3930445dba608fde6064dec9cac8794a0
 export default About;
---
+++ src/pages/home/terminal/Terminalcomp.jsx
@@ -0,0 +1,243 @@
+"use client"
+import React, { useEffect, useRef, useState } from "react"
+import { TerminalSquare } from "lucide-react"
+import { useRouter } from "next/navigation"
+
+import HelpCommand from "./HelpCommand"
+
+const Terminalcomp = () => {
+  const [input, setInput] = useState("")
+  const [commands, setCommands] = useState([])
+  const terminalRef = useRef(null)
+  const router = useRouter()
+  const getFormattedTime = () => {
+    const now = new Date()
+    let hours = now.getHours()
+    const minutes = now
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")
+    const seconds = now
+      .getSeconds()
+      .toString()
+      .padStart(2, "0")
+    const amPm = hours >= 12 ? "PM" : "AM"
+
+    hours = hours % 12 || 12
+
+    return `${hours}:${minutes}:${seconds}${amPm}`
+  }
+
+  const [currentTime, setCurrentTime] = useState(getFormattedTime())
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(() => getFormattedTime())
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
+  const handleInputChange = e => {
+    setInput(e.target.value)
+  }
+
+  const handleKeyPress = e => {
+    if (e.key === "Enter") {
+      processCommand(e)
+    }
+  }
+
+  const processCommand = async e => {
+    e.preventDefault()
+    const command = input.trim().toLowerCase()
+    let output
+    switch (command) {
+      case "hello":
+        output = "Yahallo! how can i help you??"
+        break
+      case "raj":
+        output = ""
+        window.location.href = "https://www.youtube.com/shorts/u2O1g-BEZuo"
+        break
+      case "sudo":
+        output = "missing parameters"
+        break
+      case "ls -a":
+      case "ls":
+        output = ".open_me"
+        break
+      case "cat .open_me":
+        window.location.href = "https://www.youtube.com/watch?v=hf1DkBQRQj4"
+        output = ""
+        break
+      case "shiva":
+        output =
+          "Hello there! I am Shiva, a Full Stack Developer experienced in the MERN stack, currently pursuing my Bachelor's  in Computer Science and Engineering. Type about to know more about me"
+        break
+      case "neofetch":
+        window.location.href =
+          "https://www.youtube.com/watch?v=Rl1ImG2b1k8&t=51s"
+        output = ""
+        break
+      case "about":
+        router.push("/about")
+        output = ""
+        break
+      case "blogs":
+        router.push("/blogs")
+        output = ""
+        break
+      case "proj":
+        router.push("/projects")
+        output = ""
+        break
+      case "proj ls":
+        output = (
+          <ul>
+            <li>Animetrix</li>
+            <li>Vermilion</li>
+            <li>Muxik</li>
+            <li>Quibble</li>
+            <li>Synthia</li>
+          </ul>
+        )
+        break
+      case "help":
+        output = <HelpCommand />
+        break
+      case "quibble":
+        window.location.href = "https://quibbleai.vercel.app"
+        output = ""
+        break
+      case "animetrix":
+        window.location.href = "https://animetrix.xyz"
+        output = ""
+        break
+      case "muxik":
+        window.location.href = "https://muxik.netlify.app"
+        output = ""
+        break
+      case "synthia":
+        window.location.href = "https://github.com/shivabhattacharjee/synthia"
+        output = ""
+        break
+      case "clear":
+        setCommands([])
+        output = "Terminal cleared."
+        break
+      default:
+        if (command.startsWith("echo ")) {
+          output = command.substring(5)
+        } else if (command.startsWith("rm")) {
+          window.location.href = "https://www.youtube.com/watch?v=AlLhMySQTlo"
+          output = ""
+        } else if (command.includes("apt")) {
+          output = "You are a reliable person"
+        } else if (command.includes("pacman")) {
+          output = "certified racist and a virgin (likes to go fast)"
+        } else if (command.includes("dnf")) {
+          output = "gets the job done slowly but surely"
+        } else if (/^\d+[+\-*/]\d+$/.test(command)) {
+          try {
+            const result = eval(command)
+            output = result
+          } catch (error) {
+            output = "Error in evaluating the expression."
+          }
+        } else {
+          output = `Cannot find command: ${command}. Type 'help' for more info.`
+        }
+        break
+    }
+
+    const newCommand = {
+      id: Date.now(),
+      input,
+      time: getFormattedTime(),
+      output
+    }
+
+    setCommands(prevCommands => [...prevCommands, newCommand])
+    setInput("")
+  }
+  useEffect(() => {
+    terminalRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [commands])
+
+  return (
+    <div className="border-2  border-black/30 dark:border-white/30 rounded-lg h-[450px] overflow-y-auto w-full">
+      <div className="flex p-2 justify-between mb-5 items-center sticky top-0 dark:bg-black/40 z-20 backdrop-blur-lg bg-white/40">
+        <div
+          className="flex gap-2"
+          onClick={() =>
+            (window.location.href =
+              "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+          }
+        >
+          <div className="w-3 h-3 duration-200 cursor-pointer bg-red-500 rounded-full"></div>
+          <div className="w-3 h-3 duration-200 cursor-pointer bg-yellow-500 rounded-full"></div>
+          <div className="w-3 h-3 cursor-pointer duration-200 bg-green-500 rounded-full"></div>
+        </div>
+        <h1>theshiva.xyz</h1>
+        <span className="border flex gap-1 font-medium text-sm border-white/30 rounded-lg p-2 justify-center items-center">
+          <TerminalSquare size={17} />
+          zsh
+        </span>
+      </div>
+      <div className="p-2">
+        <h1 className="text-sm font-medium  opacity-70 tracking-wide">
+          Get started by typing `help` command below
+        </h1>
+        <span className=" text-xs capitalize font-medium opacity-70 ">
+          Linux enthusiasts i have added a few easter eggs take time to discover
+        </span>
+        <br />
+        <span className=" italic mb-3 text-xs font-medium opacity-60">
+          Tip : Type any of my project name to navigate to their respective
+          hosted links
+        </span>
+        {commands.map(command => (
+          <div
+            ref={terminalRef}
+            key={command.id}
+            className="mt-2 font-mono flex  justify-between"
+            suppressHydrationWarning
+          >
+            <div className=" w-full">
+              <div className="text-md font-medium opacity-70 flex items-center gap-3 w-full">
+                {" "}
+                <span className=" text-green-500 font-bold text-2xl">
+                  {">"}
+                </span>{" "}
+                {command.input}
+              </div>
+              <span className="text-sm w-full font-medium opacity-70">
+                {command.output}
+              </span>
+            </div>
+            <span className="text-sm opacity-60">{command.time}</span>
+          </div>
+        ))}
+        <div className="flex font-mono justify-between items-center text-md">
+          <form className="w-full flex items-center gap-3">
+            <span className=" text-2xl font-bold">{">"}</span>
+            <input
+              type="text"
+              ref={inputRef}
+              value={input}
+              onChange={handleInputChange}
+              onKeyDown={e => handleKeyPress(e)}
+              className="bg-transparent w-[90%] outline-none border-none focus:outline-none"
+            />
+          </form>
+          <span className="text-sm opacity-60">{currentTime}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Terminalcomp
